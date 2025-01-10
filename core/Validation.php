79cb05eaf7643af66b82a2477a72b4dcd5441c35
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-01-10 16:02:51 +0700

    feat(validation): new class Validation
    Add packages validation, phinx, faker, carbon.
    Add Validation class is used for validating inputs.

--- core/Validation.php
+++ core/Validation.php
@@ -3,6 +3,30 @@
 namespace Core;
 
 
+use Respect\Validation\Exceptions\NestedValidationException;
+
+
 class Validation
 {
+	private array $errors = []; // Lưu các lỗi validation
+	
+	// Thực hiện validation
+	public function validate(array $data, array $rules): bool
+	{
+		foreach ($rules as $field => $rule) {
+			try {
+				$rule->assert($data[$field] ?? null);
+			} catch (NestedValidationException $e) {
+				$this->errors[$field] = $e->getMessages();
+			}
+		}
+		
+		return empty($this->errors);
+	}
+	
+	// Lấy danh sách lỗi
+	public function errors(): array
+	{
+		return $this->errors;
+	}
 }
