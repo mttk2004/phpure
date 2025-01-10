40d33eb57bbfae98ca1167b23cce3d6e936c477c
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-01-10 21:38:22 +0700

    feat(twig): integrate Twig template engine

---
+++ core/Twig.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Core;
+
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+
+class Twig
+{
+	private static ?Environment $instance = null;
+	
+	public static function getInstance(): Environment
+	{
+		if (self::$instance === null) {
+			$loader = new FilesystemLoader(BASE_PATH . '/resources/views');
+			self::$instance = new Environment($loader, [
+					'cache' => BASE_PATH . '/storage/cache',
+					'auto_reload' => true,
+			]);
+		}
+		
+		return self::$instance;
+	}
+}
