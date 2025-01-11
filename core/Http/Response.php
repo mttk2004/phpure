680b67050074b066c70c50492780eb7379a8ddec
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-01-11 15:01:33 +0700

    refactor(response): convert Response class to static methods

--- core/Http/Response.php
+++ core/Http/Response.php
@@ -2,7 +2,30 @@
 
 namespace Core\Http;
 
-
 class Response
 {
+	public static function json(array $data, int $status = 200): void
+	{
+		http_response_code($status);
+		header('Content-Type: application/json');
+		echo json_encode($data);
+	}
+	
+	public static function redirect(string $url, int $status = 302): void
+	{
+		http_response_code($status);
+		header("Location: $url");
+		exit;
+	}
+	
+	public static function send(string $content, int $status = 200): void
+	{
+		http_response_code($status);
+		echo $content;
+	}
+	
+	public static function setHeader(string $name, string $value): void
+	{
+		header("$name: $value");
+	}
 }
