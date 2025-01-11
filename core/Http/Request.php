c89b0c5ea376db49d5cdc2e27749fcc09a50bbf4
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-01-11 15:01:12 +0700

    refactor(request): convert Request class to static methods

---
+++ core/Http/Request.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Core\Http;
+
+class Request
+{
+	public static function input(string $key, $default = null)
+	{
+		return $_POST[$key] ?? $_GET[$key] ?? $default;
+	}
+	
+	public static function query(string $key, $default = null)
+	{
+		return $_GET[$key] ?? $default;
+	}
+	
+	public static function all(): array
+	{
+		return array_merge($_GET, $_POST);
+	}
+	
+	public static function method(): string
+	{
+		return strtoupper($_SERVER['REQUEST_METHOD'] ?? 'GET');
+	}
+	
+	public static function uri(): string
+	{
+		return $_SERVER['REQUEST_URI'] ?? '/';
+	}
+}
