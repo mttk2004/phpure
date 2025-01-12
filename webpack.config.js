a329f778d9f40621e6336e867669b0a5d3e292f4
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-01-12 16:21:48 +0700

    feat(ui): supports TailwindCSS and Alpine compilation with just one statement

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,32 @@
-const path = require("path");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-	entry: "./resources/js/app.js",
-	output: {
-		filename: "bundle.js",
-		path: path.resolve(__dirname, "public/js"),
+	mode   : 'development', // Change to 'production' in production environment
+	entry  : {
+		app   : './resources/js/app.js',
+		styles: './resources/css/input.css',
 	},
-	mode: "development",
+	output : {
+		filename: '[name].js',
+		path    : path.resolve(__dirname, 'public/assets'),
+		clean   : true, // Clean the output directory before build
+	},
+	module : {
+		rules: [
+			{
+				test: /\.css$/,
+				use : [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'postcss-loader', // Add postcss-loader here
+				],
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+															 filename: '[name].css',
+														 }),
+	],
 };
