193cfe1aed1321861167126ff3029db4e9e74c2e
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-01-12 17:18:23 +0700

    feat(ui): add SweetAlert2 for beautiful popup alerts

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,17 @@
 import Alpine from 'alpinejs';
+import Swal   from 'sweetalert2';
 
 
 window.Alpine = Alpine;
 Alpine.start();
+
+
+// Thêm hàm toàn cục để sử dụng SweetAlert2
+window.showAlert = (title, text, icon = 'success') => {
+	Swal.fire({
+							title,
+							text,
+							icon,
+							confirmButtonText: 'OK',
+						});
+};
