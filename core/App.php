84d0d3595e95a914ed25947d0382bf86ee04e1e3
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-01-12 16:41:53 +0700

    feat(debug): integrate Whoops for error debugging in development

--- core/App.php
+++ core/App.php
@@ -4,6 +4,8 @@ namespace Core;
 
 
 use Core\Http\Router;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
 
 
 class App
@@ -13,11 +15,23 @@ class App
 		// 1. Load môi trường
 		loadEnv();
 		
-		// 2. Khởi động session
+		// 2. Kích hoạt Whoops nếu ở môi trường development
+		if (getenv('APP_ENV') === 'development') {
+			self::enableWhoops();
+		}
+		
+		// 3. Khởi động session
 		Session::start();
 		
-		// 3. Load các file cấu hình
+		// 4. Load các file cấu hình
 		require_once BASE_PATH . '/app/events.php';
 		require_once BASE_PATH . '/app/routes.php';
+	}
+	
+	public static function enableWhoops(): void
+	{
+		$whoops = new Run();
+		$whoops->pushHandler(new PrettyPageHandler());
+		$whoops->register();
 	}
 }
