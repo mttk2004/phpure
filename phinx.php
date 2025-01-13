df186e532bc1af022f31bdf354921579c739f477
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-01-13 10:00:05 +0700

    refactor(phinx): update phinx.php using getenv() function

--- phinx.php
+++ phinx.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * Cấu hình kết nối database cho Phinx
+ *
+ * CHÚ Ý: ĐỪNG SỬA FILE NÀY.
+ * NẾU CẦN THAY ĐỔI CẤU HÌNH, HÃY SỬA FILE .ENV
+ */
+
+const BASE_PATH = __DIR__;
+require_once __DIR__ . '/utils/helpers.php';
+
+// Load các biến môi trường từ file .env để sử dụng hàm getenv()
+loadEnv();
+
 return
 [
     'paths' => [
@@ -9,33 +22,33 @@ return
     'environments' => [
         'default_migration_table' => 'phinxlog',
         'default_environment' => 'development',
-        'production' => [
-            'adapter' => 'mysql',
-            'host' => 'localhost',
-            'name' => 'production_db',
-            'user' => 'root',
-            'pass' => '',
-            'port' => '3306',
-            'charset' => 'utf8',
-        ],
+		'production' => [
+			'adapter' => getenv('DB_ADAPTER') ?? 'mysql',
+			'host' => getenv('DB_HOST') ?? 'localhost',
+			'name' => getenv('DB_NAME') ?? 'production_db',
+			'user' => getenv('DB_USER') ?? 'root',
+			'pass' => getenv('DB_PASS') ?? '',
+			'port' => getenv('DB_PORT') ?? '3306',
+			'charset' => getenv('DB_CHARSET') ?? 'utf8mb4',
+		],
         'development' => [
-            'adapter' => 'mysql',
-            'host' => 'localhost',
-            'name' => 'my_database',
-            'user' => 'kiet',
-            'pass' => 'password',
-            'port' => '3306',
-            'charset' => 'utf8',
+            'adapter' => getenv('DB_ADAPTER') ?? 'mysql',
+            'host' => getenv('DB_HOST') ?? 'localhost',
+            'name' => getenv('DB_NAME') ?? 'development_db',
+            'user' => getenv('DB_USER') ?? 'root',
+            'pass' => getenv('DB_PASS') ?? '',
+            'port' => getenv('DB_PORT') ?? '3306',
+            'charset' => getenv('DB_CHARSET') ?? 'utf8mb4',
         ],
-        'testing' => [
-            'adapter' => 'mysql',
-            'host' => 'localhost',
-            'name' => 'testing_db',
-            'user' => 'root',
-            'pass' => '',
-            'port' => '3306',
-            'charset' => 'utf8',
-        ]
+		'testing' => [
+			'adapter' => getenv('DB_ADAPTER') ?? 'mysql',
+			'host' => getenv('DB_HOST') ?? 'localhost',
+			'name' => getenv('DB_NAME') ?? 'testing_db',
+			'user' => getenv('DB_USER') ?? 'root',
+			'pass' => getenv('DB_PASS') ?? '',
+			'port' => getenv('DB_PORT') ?? '3306',
+			'charset' => getenv('DB_CHARSET') ?? 'utf8mb4',
+		],
     ],
     'version_order' => 'creation'
 ];
