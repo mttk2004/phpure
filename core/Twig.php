65683765dd60cbe5878ba80e304e15d81d3700b9
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-04-03 16:07:18 +0700

    feat(core): add addHelpers() method to Twig class

--- core/Twig.php
+++ core/Twig.php
@@ -3,6 +3,7 @@
 namespace Core;
 
 use Twig\Environment;
+use Twig\TwigFunction;
 use Twig\Loader\FilesystemLoader;
 
 class Twig
@@ -17,8 +18,36 @@ class Twig
                     'cache' => BASE_PATH . '/storage/cache',
                     'auto_reload' => true,
             ]);
+
+            // Thêm các hàm tiện ích
+            self::addHelpers();
         }
 
         return self::$instance;
+    }
+
+    private static function addHelpers(): void
+    {
+        $instance = self::$instance;
+
+        // Thêm hàm asset
+        $instance->addFunction(new TwigFunction('asset', function ($path) {
+            return '/assets/' . ltrim($path, '/');
+        }));
+
+        // Thêm hàm url
+        $instance->addFunction(new TwigFunction('url', function ($path) {
+            return '/' . ltrim($path, '/');
+        }));
+
+        // Thêm hàm session
+        $instance->addFunction(new TwigFunction('session', function ($key, $default = null) {
+            return Session::get($key, $default);
+        }));
+
+        // Thêm hàm flash
+        $instance->addFunction(new TwigFunction('flash', function ($key) {
+            return Session::flash($key);
+        }));
     }
 }
