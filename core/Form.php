7b59d2db3e8aee1b3d938ad01e01a2892bd59cc5
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-04-03 16:00:11 +0700

    feat(core): add Form class - easy create and handle form

---
+++ core/Form.php
@@ -0,0 +1,31 @@
+<?php
+namespace Core;
+
+class Form
+{
+    // Tạo token CSRF
+    public static function token(): string
+    {
+        $token = bin2hex(random_bytes(32));
+        Session::set('csrf_token', $token);
+        return $token;
+    }
+
+    // Xác thực token CSRF
+    public static function validateToken(string $token): bool
+    {
+        $csrfToken = Session::get('csrf_token');
+        return $token === $csrfToken;
+    }
+
+    // Tạo input field
+    public static function input(string $type, string $name, string $value = '', array $attributes = []): string
+    {
+        $attrs = '';
+        foreach ($attributes as $key => $val) {
+            $attrs .= " $key=\"$val\"";
+        }
+
+        return "<input type=\"$type\" name=\"$name\" value=\"$value\"$attrs>";
+    }
+}
