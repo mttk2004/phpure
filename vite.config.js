1bbd1ab17e0bc0209179ab0359f351961a464adf
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-04-06 09:25:27 +0700

    chore: uninstall unnecessary dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import legacy from '@vitejs/plugin-legacy';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   build: {
@@ -19,6 +20,7 @@ export default defineConfig({
     }
   },
   plugins: [
+    tailwindcss(),
     legacy({
       targets: ['defaults', 'not IE 11']
     })
